e3399d6f0e400ae40cee1c4a23faaab9d0c637d3
omer <peynirciomerfaruk@gmail.com>
2022-05-11 12:20:07 +0200

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-
-import './App.scss'
+import "./App.scss";
 
 function App() {
-  
-
   return (
     <div className="App">
-      
+      <h1>Personal Site</h1>
+      <nav>
+        <ul>
+          <li>Welcome |</li>
+          <li>Books |</li>
+          <li>About</li>
+        </ul>
+      </nav>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
