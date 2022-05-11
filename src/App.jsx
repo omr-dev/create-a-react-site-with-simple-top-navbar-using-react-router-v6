3c96840e45284992fbb0f402d0830c3a97a1e96d
omer <peynirciomerfaruk@gmail.com>
2022-05-11 12:41:51 +0200

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.scss";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
 import { PageWelcome } from "./pages/PageWelcome";
 import { PageBooks } from "./pages/PageBooks";
@@ -9,9 +10,12 @@ function App() {
     <div className="App">
       <h1>Personal Site</h1>
       <Navbar />
-      <PageWelcome />
-      <PageBooks />
-      <PageAbout />
+      <Routes>
+        <Route path="/" element={<Navigate to="/welcome" replace />} />
+        <Route path="/welcome" element={<PageWelcome />} />
+        <Route path="/books" element={<PageBooks />} />
+        <Route path="/about" element={<PageAbout />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,17 @@
+import { NavLink } from "react-router-dom";
 export function Navbar() {
   return (
     <nav>
       <ul>
-        <li>Welcome |</li>
-        <li>Books |</li>
-        <li>About</li>
+        <li>
+          <NavLink to="/welcome">Welcome</NavLink> |
+        </li>
+        <li>
+          <NavLink to="/books">Books</NavLink> |
+        </li>
+        <li>
+          <NavLink to="/about">About</NavLink>
+        </li>
       </ul>
     </nav>
   );
